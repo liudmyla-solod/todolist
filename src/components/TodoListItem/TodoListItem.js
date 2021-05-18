fdc39578c8a7fd76f8588f24c37211d9ec988db4
Solod Liudmyla <solodludmila@gmail.com>
2021-05-18 14:20:13 +0200

    change ItemStatusFilter and TodoListItem function components to class components

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './TodoListItem.css'
 
-const TodoListItem = ({ label, important = false }) => {
+class TodoListItem extends Component{
+
+  render() {
+    const { label, important = false }= this.props;
+
+    const style = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    };
+
+    return (
+      <span className="todo-list-item">
+      <span
+        className="todo-list-item-label"
+        style={style}>
+        {label}
+      </span>
+
+      <button type="button"
+              className="btn btn-outline-success btn-sm pull-right">
+        <i className="fa fa-exclamation" />
+      </button>
+
+      <button type="button"
+              className="btn btn-outline-danger btn-sm pull-right">
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+    );
+  }
+}
+
+export default TodoListItem;
+
+/*const TodoListItemFunc = ({ label, important = false }) => {
+  const { label, important = false }= this.props;
   const style = {
     color: important ? 'steelblue' : 'black',
     fontWeight: important ? 'bold' : 'normal'
@@ -25,6 +60,4 @@ const TodoListItem = ({ label, important = false }) => {
       </button>
     </span>
   );
-};
-
-export default TodoListItem;
+};*/
